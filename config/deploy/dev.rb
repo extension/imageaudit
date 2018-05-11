bd2dde0ee504156909e312f49f79e1b052afc0fd
Jason Young <jay@outfielding.net>
2018-05-11 11:24:11 -0400

    remove dev deploy target

--- config/deploy/dev.rb
+++
@@ -1,9 +0,0 @@
-set :deploy_to, "/services/imageaudit/"
-if(branch = ENV['BRANCH'])
-  set :branch, branch
-else
-  set :branch, 'master'
-end
-set :vhost, 'dev-imageaudit.extension.org'
-set :deploy_server, 'dev-imageaudit.awsi.extension.org'
-server deploy_server, :app, :web, :db, :primary => true
